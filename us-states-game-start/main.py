cba19efd77c96c9eb4e52c9e154dc7290cfaf24f
ykarimi <ykarimi.2312@gmail.com>
2021-03-22 21:13:03 -0700

    last commit

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -3,14 +3,13 @@ import pandas
 
 screen = turtle.Screen()
 count = 0
-game_is_on = True
 screen.title("U.S. states game")
 screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 correct_guess = []
 
-while game_is_on:
-    user_guess = screen.textinput(title=f"{count}/50 correct states", prompt="What's another state?")
+while len(correct_guess) < 50:
+    user_guess = screen.textinput(title=f"{count}/50 correct states", prompt="What's another state?").title()
     data = pandas.read_csv("50_states.csv")
     if user_guess in data.values:
         correct_guess.append(user_guess)
@@ -24,8 +23,17 @@ while game_is_on:
         new_turtle.goto(x, y)
         new_turtle.write(user_guess)
 
-        print(x,y)
-    if count == 50 :
-        game_is_on = False
+
+    if user_guess.casefold() == "exit".casefold():
+
+        all_states = data.state.to_list()
+        learn = []
+        for state in all_states:
+            if state not in correct_guess:
+                learn.append(state)
+        dataframe = pandas.DataFrame(learn)
+        dataframe.to_csv("states_to_learn.csv")
+
+        break
 turtle.mainloop()
 screen.exitonclick()
